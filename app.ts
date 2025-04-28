35bf32b867030cc9364678a4e37421970cc595bf
Cyrus-0101 <cyrus_g@outlook.com>
2025-04-28 23:18:35 +0300

    refactor: Refactor engine initialization and remove unused code

--- app.ts
+++ app.ts
@@ -1,24 +1,10 @@
-namespace KoruTS {
-  export class Engine {
-    private _count: number = 0;
+import { KoruTSEngine } from "./engine";
 
-    public constructor() {}
+let engine: KoruTSEngine;
 
-    public start(): void {
-      this.loop();
-    }
-
-    private loop(): void {
-      this._count++;
-
-      document.title = this._count.toString();
-      requestAnimationFrame(this.loop.bind(this));
-    }
-  }
-}
-
+// Initialize engine when the window loads
 window.onload = function () {
-  let e = new KoruTS.Engine();
-  e.start();
-  document.body.innerHTML += "Foo";
+  engine = new KoruTSEngine();
+
+  engine.start();
 };
