b93719c55db033e5b86467191a763e6ec624e997
Cyrus-0101 <cyrus_g@outlook.com>
2025-05-05 10:55:09 +0300

    feat: Update fragment shader to use tint and texture sampling

--- src/core/shaders/basic.frag.ts
+++ src/core/shaders/basic.frag.ts
@@ -5,8 +5,12 @@
 export const fragmentShaderSource = `
 precision mediump float;
 
-uniform vec4 u_color;
+uniform vec4 u_tint;
+
+uniform sampler2D u_diffuse;
+
+varying vec2 v_texCoord;
 
 void main() {
-    gl_FragColor = u_color;
+    gl_FragColor = u_tint * texture2D(u_diffuse, v_texCoord);
 }`;
