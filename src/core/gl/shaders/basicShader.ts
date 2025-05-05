5ad59d00b96e466cfa165da4dfc34d7c17e23576
Cyrus-0101 <cyrus_g@outlook.com>
2025-05-05 20:46:50 +0300

    feat: Implement BasicShader for 2D rendering with texture and color support

---
+++ src/core/gl/shaders/basicShader.ts
@@ -0,0 +1,35 @@
+import { fragmentShaderSource } from "../../shaders/basic.frag";
+import { vertexShaderSource } from "../../shaders/basic.vert";
+import { Shader } from "../shaders";
+
+/**
+ * BasicShader - Default shader implementation for 2D rendering
+ *
+ * Features:
+ * - Basic vertex transformation
+ * - Texture coordinate handling
+ * - Color tinting support
+ *
+ * Uniforms:
+ * - u_projection: Projection matrix
+ * - u_model: Model transformation matrix
+ * - u_tint: Color tint (vec4)
+ *
+ * Attributes:
+ * - a_position: Vertex position (vec3)
+ * - a_texCoord: Texture coordinates (vec2)
+ */
+export class BasicShader extends Shader {
+  public constructor() {
+    super("basic");
+    this.load(this.getVertexSource(), this.getFragmentSource());
+  }
+
+  private getVertexSource(): string {
+    return vertexShaderSource;
+  }
+
+  private getFragmentSource(): string {
+    return fragmentShaderSource;
+  }
+}
