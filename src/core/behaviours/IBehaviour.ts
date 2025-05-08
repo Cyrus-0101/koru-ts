9b74ef30e18cdac131565779e1ed1c446e4c23ce
Cyrus-0101 <cyrus_g@outlook.com>
2025-05-08 18:45:21 +0300

    feat: Implemented IBehaviour interface for definining behaviour core functionality

---
+++ src/core/behaviours/IBehaviour.ts
@@ -0,0 +1,32 @@
+import type { SimObject } from "../world/simObject";
+
+/**
+ * IBehaviour - Interface defining core behaviour functionality
+ *
+ * Required Methods:
+ * - setOwner: Assigns owning game object
+ * - update: Processes behaviour logic
+ * - apply: Activates behaviour effects
+ */
+export interface IBehaviour {
+  /** Unique name identifier */
+  name: string;
+
+  /**
+   * Sets the owning game object
+   * @param owner SimObject that owns this behaviour
+   */
+  setOwner(owner: SimObject): void;
+
+  /**
+   * Updates behaviour state
+   * @param time Current engine time in milliseconds
+   */
+  update(time: number): void;
+
+  /**
+   * Applies behaviour effects
+   * @param userData Optional customization data
+   */
+  apply(userData: any): void;
+}
