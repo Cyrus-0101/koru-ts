de300d8779df5cc42e6ed86fae268ebb51a56c8f
Cyrus-0101 <cyrus_g@outlook.com>
2025-05-11 10:43:55 +0300

    feat: Added origin and made offset readonly, added docs

--- src/core/graphics/shapes2D/IShape2D.ts
+++ src/core/graphics/shapes2D/IShape2D.ts
@@ -1,7 +1,15 @@
 import type { Vector2 } from "../../math/vector2";
 
+/**
+ * IShape2D - Interface for shapes.
+ * Defines position, origin, offset and other methods
+ */
 export interface IShape2D {
   position: Vector2;
+
+  origin: Vector2;
+
+  readonly offset: Vector2;
 
   setFromJson(json: any): void;
 
