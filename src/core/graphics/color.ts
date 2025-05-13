82ad3f22540732663040220defa445a605507869
Cyrus-0101 <cyrus_g@outlook.com>
2025-05-13 20:21:11 +0300

    fix: Bug - Changed private to public methods

--- src/core/graphics/color.ts
+++ src/core/graphics/color.ts
@@ -38,61 +38,61 @@ export class Color {
   }
 
   /** Gets red component (0-255) */
-  private get r(): number {
+  public get r(): number {
     return this._r;
   }
 
   /** Gets red component as float (0.0-1.0) for WebGL */
-  private get rFloat(): number {
+  public get rFloat(): number {
     return this._r / 255.0;
   }
 
   /** Sets red component (0-255) */
-  private set r(value: number) {
+  public set r(value: number) {
     this._r = value;
   }
   /** Gets green component (0-255) */
-  private get g(): number {
+  public get g(): number {
     return this._g;
   }
 
   /** Gets green component as float (0.0-1.0) for WebGL */
-  private get gFloat(): number {
+  public get gFloat(): number {
     return this._g / 255.0;
   }
 
   /** Sets green component (0-255) */
-  private set g(value: number) {
+  public set g(value: number) {
     this._g = value;
   }
 
   /** Gets blue component (0-255) */
-  private get b(): number {
+  public get b(): number {
     return this._b;
   }
 
   /** Gets blue component as float (0.0-1.0) for WebGL */
-  private get bFloat(): number {
+  public get bFloat(): number {
     return this._b / 255.0;
   }
 
   /** Sets blue component (0-255) */
-  private set b(value: number) {
+  public set b(value: number) {
     this._b = value;
   }
 
   /** Gets alpha component (0-255) */
-  private get a(): number {
+  public get a(): number {
     return this._a;
   }
 
   /** Gets alpha component as float (0.0-1.0) for WebGL */
-  private get aFloat(): number {
+  public get aFloat(): number {
     return this._a / 255.0;
   }
 
   /** Sets alpha component (0-255) */
-  private set a(value: number) {
+  public set a(value: number) {
     this._a = value;
   }
 
