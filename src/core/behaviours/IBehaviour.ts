0e3a4bca1226f8e3de93b9bb676ee5d3cf42ce82
Cyrus-0101 <cyrus_g@outlook.com>
2025-05-13 09:40:28 +0300

    feat: Added updateReady that assists in avoiding race conditions in resource loading

--- src/core/behaviours/IBehaviour.ts
+++ src/core/behaviours/IBehaviour.ts
@@ -19,6 +19,12 @@ export interface IBehaviour {
   setOwner(owner: SimObject): void;
 
   /**
+   * Avoids race conditions
+   * Helps us wait until update is ready
+   */
+  updateReady(): void;
+
+  /**
    * Updates behaviour state
    * @param time Current engine time in milliseconds
    */
