550cb26c5a5703e02cd46acb6e7c4411d6d6170a
Cyrus-0101 <cyrus_g@outlook.com>
2025-05-14 19:53:41 +0300

    fix: Bug fix, only listen to messages if the asset is defined

--- src/core/graphics/texture.ts
+++ src/core/graphics/texture.ts
@@ -59,8 +59,6 @@ export class Texture implements IMessageHandler {
 
     this._handle = gl.createTexture();
 
-    Message.subscribe(MESSAGE_ASSET_LOADER_ASSET_LOADED + this._name, this);
-
     this.bind();
 
     // Used when loading raw data to the texture
@@ -81,6 +79,8 @@ export class Texture implements IMessageHandler {
 
     if (asset !== undefined) {
       this.loadTextureFromAsset(asset);
+    } else {
+      Message.subscribe(MESSAGE_ASSET_LOADER_ASSET_LOADED + this._name, this);
     }
   }
 
